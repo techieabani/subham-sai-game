fec7dd565daceb9403a34ea5b9fbc65fe42e88b5
Abani Sahoo <55310811+techieabani@users.noreply.github.com>
2020-06-04 21:10:55 +0530

    Updated game.js

--- game.js
+++ game.js
@@ -24,11 +24,13 @@ function startGame() {
 }
 
 $(".btn").click(function(){
+  if (isGameStarted) {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   animatePress(userChosenColour);
   checkAnswer(userClickedPattern.length-1);
+  }
 });
 
 function checkAnswer(currentLevel) {
